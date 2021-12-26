a16c6ec50d3a364093ac640eff152f58b403e3d8
Kuba Biel <jakubiel@o2.pl>
2021-12-26 12:35:41 +0100

    s

--- req.js
+++ req.js
@@ -1,26 +1,29 @@
-const firstReq = new XMLHttpRequest();
-firstReq.addEventListener("load", (res) => {
-	console.log("FIRST works");
-	const data = JSON.parse(firstReq.responseText);
-	const filmURL = data.results[0].films[0];
-	const filmReq = new XMLHttpRequest();
-	filmReq.addEventListener("load", function () {
-		console.log("SECOND works");
-		const filmData = JSON.parse(filmReq.responseText);
-		console.log(filmData);
-	});
-	filmReq.addEventListener("error", function (e) {
-		console.log("error", e);
-	});
-	filmReq.open("GET", filmURL);
-	filmReq.send();
-	// for (let planet of data.results) {
-	// 	console.log(planet.name);
-	// }
-});
-firstReq.addEventListener("error", () => {
-	console.log("error");
-});
-firstReq.open("GET", "https://swapi.dev/api/planets/");
-firstReq.send();
-console.log("request sent");
+// const firstReq = new XMLHttpRequest();
+// firstReq.addEventListener("load", (res) => {
+// 	console.log("FIRST works");
+// 	const data = JSON.parse(firstReq.responseText);
+// 	const filmURL = data.results[0].films[0];
+// 	const filmReq = new XMLHttpRequest();
+// 	filmReq.addEventListener("load", function () {
+// 		console.log("SECOND works");
+// 		const filmData = JSON.parse(filmReq.responseText);
+// 		console.log(filmData);
+// 	});
+// 	filmReq.addEventListener("error", function (e) {
+// 		console.log("error", e);
+// 	});
+// 	filmReq.open("GET", filmURL);
+// 	filmReq.send();
+// 	// for (let planet of data.results) {
+// 	// 	console.log(planet.name);
+// 	// }
+// });
+// firstReq.addEventListener("error", () => {
+// 	console.log("error");
+// });
+// firstReq.open("GET", "https://swapi.dev/api/planets/");
+// firstReq.send();
+// console.log("request sent");
+
+//FETCH
+https://swapi.dev/api/planets/
