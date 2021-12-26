244aa2801b28697b7ef33e6e251ec87a48b57936
Kuba Biel <jakubiel@o2.pl>
2021-12-26 21:18:57 +0100

    s

--- async.js
+++ async.js
@@ -11,3 +11,17 @@ async function add(x, y) {
 
 	return x + y;
 }
+
+//
+// function getPlanets() {
+// 	return axios.get("https://swapi.dev/api/planets/");
+// }
+// getPlanets().then((res) => {
+// 	console.log(res.data);
+// });
+
+async function getPlanets() {
+	const res = await axios.get("https://swapi.dev/api/planets/");
+	console.log(res.data);
+}
+getPlanets();
