1f72a82ee8224bbca71b0e153d8ead97ce6b7459
Kuba Biel <jakubiel@o2.pl>
2021-12-26 20:08:09 +0100

    s

---
+++ axios.js
@@ -0,0 +1,38 @@
+// axios
+// 	.get("https://swapi.dev/api/planets/")
+// 	.then((res) => {
+// 		console.log(res.data.results);
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 	});
+
+const nextPlanets = (url = "https://swapi.dev/api/planets/") => {
+	return axios.get(url);
+};
+
+const printPlanets = ({ data }) => {
+	console.log({ data });
+	for (let planet of data.results) {
+		console.log(planet.name);
+	}
+	// return nextPlanets(data.next);  and no additional .then(nextPlanets)
+	return Promise.resolve(data.next);
+};
+
+nextPlanets()
+	.then(printPlanets)
+	.then(nextPlanets)
+	.then(printPlanets)
+	.then(nextPlanets)
+	.then(printPlanets)
+	.then(nextPlanets)
+	.then(printPlanets)
+	.then(nextPlanets)
+	.then(printPlanets)
+	.then(nextPlanets)
+	.then(printPlanets)
+	.then(nextPlanets)
+	.catch((err) => {
+		console.log(err);
+	});
