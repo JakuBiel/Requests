12ba20b7798fe20d4e5f2814064e677e4837640a
Kuba Biel <jakubiel@o2.pl>
2021-12-27 17:55:29 +0100

    5

--- async.js
+++ async.js
@@ -20,8 +20,53 @@ async function add(x, y) {
 // 	console.log(res.data);
 // });
 
-async function getPlanets() {
-	const res = await axios.get("https://swapi.dev/api/planets/");
-	console.log(res.data);
+// async function getPlanets() {
+// 	const res = await axios.get("https://swapi.dev/api/planetss/");
+// 	console.log(res.data);
+// }
+// getPlanets();
+
+// getPlanets().catch((err) => {
+// 	console.log("in catch");
+// 	console.log(err);
+// });
+
+// async function getPlanets() {
+// 	try {
+// 		const res = await axios.get("https://swapi.dev/api/planets/");
+// 		console.log(res.data);
+// 	} catch (e) {
+// 		console.log("in catch");
+// 		console.log(e);
+// 	}
+// }
+
+const btn = document.querySelector("button");
+
+const moveX = (element, amount, delay) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const bodyBoundary = document.body.clientWidth;
+			const elRight = element.getBoundingClientRect().right;
+			const currLeft = element.getBoundingClientRect().left;
+			if (elRight + amount > bodyBoundary) {
+				reject();
+			} else {
+				element.style.transform = `translateX(${currLeft + amount}px)`;
+				resolve();
+			}
+		}, delay);
+	});
+};
+
+async function moveRight(el) {
+	await moveX(el, 100, 1000);
+	await moveX(el, 100, 1000);
+	await moveX(el, 100, 1000);
+	await moveX(el, 100, 1000);
+	await moveX(el, 100, 1000);
+	await moveX(el, 100, 1000);
+	moveX(el, 100, 1000);
 }
-getPlanets();
+moveRight(btn).catch((err) => console.log("Done!", err));
+moveRight(btn);
