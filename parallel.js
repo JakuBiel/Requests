4f8ff293c8102ac81ffd6b24d83dc72399f4246a
Kuba Biel <jakubiel@o2.pl>
2021-12-27 18:42:21 +0100

    w

--- parallel.js
+++ parallel.js
@@ -5,11 +5,20 @@
 // }
 
 //parallel, get at one
+// async function get3Pokemon() {
+// 	const promise1 = axios.get("https://pokeapi.co/api/v2/pokemon/1");
+// 	const promise2 = axios.get("https://pokeapi.co/api/v2/pokemon/2");
+// 	const poke1 = await promise1; //p1 is a promise, poke1 is data if resolved
+// 	const poke2 = await promise2;
+// 	console.log(poke1.data.name + ", " + poke2.data.name);
+// }
+// get3Pokemon();
+
 async function get3Pokemon() {
 	const promise1 = axios.get("https://pokeapi.co/api/v2/pokemon/1");
 	const promise2 = axios.get("https://pokeapi.co/api/v2/pokemon/2");
-	const poke1 = await promise1; //p1 is a promise, poke1 is data if resolved
-	const poke2 = await promise2;
-	console.log(poke1.data.name + ", " + poke2.data.name);
+	const results = await Promise.all([promise1, promise2]);
+
+	console.log(results);
 }
 get3Pokemon();
